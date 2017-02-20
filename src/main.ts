b152088261e63cf194e3b843dfbce847a4baae8a
lenovo\lenovo-pc <2213286021@qq.com>
2017-02-20 21:14:30 +0900

    添加矩阵

--- src/main.ts
+++ src/main.ts
@@ -5,12 +5,12 @@ window.onload = () => {
     var canvas = document.getElementById("app") as HTMLCanvasElement;
     var context2D = canvas.getContext("2d");
 
-    
+
 
     context2D.fillStyle = "#FFFF0000";
     context2D.strokeStyle = "#00FF00";
 
-    context2D.setTransform(1,0,0,1,50,50);
+    context2D.setTransform(1, 0, 0, 1, 50, 50);
     context2D.rect(0, 0, 100, 100);
     context2D.fill();
 
@@ -19,8 +19,8 @@ window.onload = () => {
     //image.draw(context2D);
 
     setInterval(() => {
-            context2D.clearRect(0, 0, canvas.width, canvas.height);
-            stage.draw(context2D);
+        context2D.clearRect(0, 0, canvas.width, canvas.height);
+        stage.draw(context2D);
     }, 30);
 
     var text = new TextField();
@@ -29,14 +29,14 @@ window.onload = () => {
 
     var image = document.createElement("img");
     image.src = "assets/monster.jpg"
-   
+
     image.onload = () => {
 
         var exo = new Bitmap();
         exo.image = image;
         exo.width = 100;
         exo.height = 100;
-        
+
         stage.addChild(exo);
         stage.addChild(text);
 
@@ -50,17 +50,32 @@ window.onload = () => {
 
 interface Drawable {
 
+    parent:DisplayObjectContainer;
+
     draw(context2D: CanvasRenderingContext2D);
 
 }
 
-class DisplayObject implements Drawable{
+class DisplayObject implements Drawable {
 
-    x:number = 0;
-    y:number = 0;
+    x: number = 0;
+    y: number = 0;
+    scaleX: number = 1;
+    scaleY: number = 1;
+    rotation = 0;
+    absoluteMatrix: math.Matrix = new math.Matrix();
+    parent: DisplayObjectContainer = new DisplayObjectContainer();
 
-    draw(context2D: CanvasRenderingContext2D){
-
+    draw(context2D: CanvasRenderingContext2D) {
+        var matrix: math.Matrix = new math.Matrix();
+        matrix.updateFromDisplayObject(this.x, this.y, this.scaleX, this.scaleY, this.rotation);
+        if (this.parent) {
+            var parentMatrix: math.Matrix = this.parent.absoluteMatrix;
+            this.absoluteMatrix = math.matrixAppendMatrix(this.absoluteMatrix, parentMatrix);
+        } else {
+            this.absoluteMatrix = matrix;
+            context2D.setTransform(matrix.m11, matrix.m12, matrix.m21, matrix.m22, matrix.dx, matrix.dy);
+        }
     }
 
 }
@@ -68,20 +83,24 @@ class DisplayObject implements Drawable{
 class Bitmap extends DisplayObject {
 
     image: HTMLImageElement;
-    width:number = 0;
-    height:number = 0;
+    width: number = 0;
+    height: number = 0;
 
     draw(context2D: CanvasRenderingContext2D) {
-        context2D.drawImage(this.image, this.x,this.y,this.width,this.height);
+        context2D.drawImage(this.image, this.x, this.y, this.width, this.height);
+    }
+
+    scale() {
+
     }
 }
 
 
 
-class TextField extends DisplayObject{
+class TextField extends DisplayObject {
 
     text: string = "";
-    color:string = "";
+    color: string = "";
 
     draw(context2D: CanvasRenderingContext2D) {
         context2D.fillStyle = this.color;
@@ -90,7 +109,7 @@ class TextField extends DisplayObject{
     }
 }
 
-class DisplayObjectContainer implements Drawable {
+class DisplayObjectContainer extends DisplayObject {
 
     array: Drawable[] = [];
 
@@ -101,6 +120,7 @@ class DisplayObjectContainer implements Drawable {
     }
 
     addChild(child: Drawable) {
+        child.parent = this;
         this.array.push(child);
     }
 
